1743184ddf3445a3709c35c1317feb60f5171520
Goutam-Xtremsoft <goutam@xtremsoftindia.com>
2020-10-06 22:51:32 +0530

    initial commit

---
+++ app/src/main/java/com/stark/understandparcelable/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stark.understandparcelable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
